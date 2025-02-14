72e191251098d7109629ff459d573ab889431992
saipatibandla <sdpa14@gmail.com>
2025-02-14 15:32:35 -0600

    Add more movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,24 @@
 #   end
 
 more_movies = [
-  { title: 'My Neighbor Totoro', rating: 'G',
-    release_date: '16-Apr-1988' },
-  { title: 'Green Book', rating: 'PG-13',
+    # { title: 'My Neighbor Totoro', rating: 'G',
+    #   release_date: '16-Apr-1988' },
+    # { title: 'Green Book', rating: 'PG-13',
+    #   release_date: '16-Nov-2018' },
+    # { title: 'Parasite', rating: 'R',
+    #   release_date: '30-May-2019' },
+    # { title: 'Nomadland', rating: 'R',
+    #   release_date: '19-Feb-2021' },
+    # { title: 'CODA', rating: 'PG-13',
+    #   release_date: '13-Aug-2021' },
+    { title: 'Titanic', rating: 'PG-13',
+    release_date: '19-Dec-1997' },
+  { title: 'Inception', rating: 'PG-13',
     release_date: '16-Nov-2018' },
-  { title: 'Parasite', rating: 'R',
-    release_date: '30-May-2019' },
-  { title: 'Nomadland', rating: 'R',
-    release_date: '19-Feb-2021' },
-  { title: 'CODA', rating: 'PG-13',
-    release_date: '13-Aug-2021' }
+  { title: 'Dune: Part Two', rating: 'PG-13',
+    release_date: '01-Mar-2024' },
+  { title: 'The Martian', rating: 'PG-13',
+    release_date: '02-Oct-2015' }
 ]
 
 more_movies.each do |movie|
